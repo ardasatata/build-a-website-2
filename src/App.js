00e1f67647e2a6023f0e5737ef5d21235dc68cf1
Arda Satata <ardasatata@gmail.com>
2020-12-29 11:57:53 +0800

    add an img1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
+import img1 from './assets/img1.jpg';
 
 function App() {
   return (
     <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
-        <p className="text-2xl">
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-sm font-light"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is the weather-app starter code for Build a Website II
-        </a>
+        <div>
+          <img src={img1} />
+        </div>
+        <a>Hello my name is Arda</a>
       </header>
     </div>
   );
